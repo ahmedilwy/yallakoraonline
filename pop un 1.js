1866fe58e2ca18156eb6e6862aaf16310dde5d5a
a.ilwy93@gmail.com <a.ilwy93@gmail.com@5c1807b5-d69c-7330-cbf7-d59c4dde58f7>
2014-11-28 21:47:29 +0000

    فتحة اجبارية بكل ضغطة

---
+++ pop un 1.js
@@ -0,0 +1,4 @@
+<script type="text/javascript">
+document.body.onclick= function(){
+window.open('http://a.adorika.net/c/banner_s?tenant=AD&selection=4856&size=UN&skin=pop&auto_click=1&di=1', 'mywindow' , 'toolbars=0, scrollbars=1, location=0, statusbars=0, menubars=0, resizable=1, width=550, height=700, left = 0, top = 50'); 
+}</script>
